7d0f65eded6a5593f18b8ba0549649dae595ba57
diego <32820519+DIEGO32OF@users.noreply.github.com>
2019-02-25 23:55:53 -0600

    Update comandas.js

--- controllers/comandas.js
+++ controllers/comandas.js
@@ -136,7 +136,7 @@ function guardaCodigoCocina(req,res){
     
    var mycode=new Codigos();
     var myparames=req.body;
-	console.log(myparames);
+	console.log(req.body);
     mycode.Codigo=myparames.Codigo;
     mycode.Mesa=myparames.Mesa;
    mycode.Local=myparames.Local;
