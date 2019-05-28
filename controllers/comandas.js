baa7c57c55250a9fce50bd7cdba0287edc396084
diego <32820519+DIEGO32OF@users.noreply.github.com>
2019-05-28 13:11:57 -0500

    Update comandas.js

--- controllers/comandas.js
+++ controllers/comandas.js
@@ -81,10 +81,16 @@ function payComand(req, res){
     if(err)
     res.status(500).send({message:'error al actualizar la comanda'+err});
     else{
+	    if(parametros.Codigo!=''){
       Codigos.findByIdAndUpdate(parametros.Codigo,{status:'Cerrado'},(err,Code)=>{
         if(err)
         res.status(500).send({message:'error al actualizar el Codigo'+err});
+	      else
+		      res.status(200).send({message:'Se actualizo correctamente el codigo y comanda'});
       });
+	    }
+	    else
+		    res.status(200).send({message:'Se actualizo correctamente la comanda'});
     }
   });
 }
