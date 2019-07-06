9841e7bc8d22c6ddb82cb2b3befae22be8943869
diego <32820519+DIEGO32OF@users.noreply.github.com>
2019-07-05 23:11:06 -0500

    Add files via upload

---
+++ services/graphAPI.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const request = require('request');
+
+
+exports.callSendAPI = (requestBody) => {
+    const url = 'https://graph.facebook.com/v3.3/me/messages';
+    request({
+        uri=url,
+        qs: { acces_token: process.env.ACCES_TOKEN, },
+        method: 'POST',
+        json: requestBody,
+    }, (error, Body) => {
+        if (!error) {
+            console.log('peticion enviada', Body);
+        }
+        else {
+            console.log('no se pudo enviar la peticion', error);
+        }
+    );
+}
