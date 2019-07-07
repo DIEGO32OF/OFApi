51b0e08977619cba24eaf95da12ba10f1050dcf5
diego <32820519+DIEGO32OF@users.noreply.github.com>
2019-07-07 13:21:32 -0500

    Update handleMessages.js

--- services/handleMessages.js
+++ services/handleMessages.js
@@ -12,5 +12,27 @@ exports.handleMessage = (webhookEvent) => {
         else if (mensaje.text) {
             actions.sendTextMessage('has enviado texto', webhookEvent);
         }
+         else if (webhookEvent.postback) {
+            handlePostback
+        }
+    }
+}
+
+handlePostback = (webhookEvent) => {
+    let evento = webhookEvent.postback.payload;
+    switch (evento) {
+        case 'encuestas':
+            console.log('se eligio las encuestas ');
+            break;
+
+
+        case 'Informacion':
+            actions.sendTextMessage('oprimiste mas info', webhookEvent);
+            break;
+
+        case 'inicio':
+            actions.sendTextMessage('Hola Binevenido a ordenofacil, yo te ayudo', webhookEvent);
+            break;
+
     }
 }
