fc1929dbb95e37584cdde12d1ccd1e8dc23456ab
diego <32820519+DIEGO32OF@users.noreply.github.com>
2019-07-08 18:29:56 -0500

    Update actions.js

--- services/actions.js
+++ services/actions.js
@@ -51,3 +51,43 @@ exports.sendTextMessage = (texto, webhookEvent) => {
     sendAPI.getProfile(webhookEvent.sender.id);
     sendAPI.callSendAPI(response);
 }
+
+exports.ubicacion = (webhookEvent) => {
+    let response = {
+        recipient={
+            id: webhookEvent.sender.id
+        },
+        message: {
+            attachment: {
+                type: 'template',
+                payload: {
+                    template_type: 'generic',
+                    elements: [{
+                        title: 'tacos jarochos',
+                        image_url: 'http://ordenofacil.com/logos/coca.jpg',
+                        subtitle: 'direccion corta de los tacos',
+                        default_action: {
+                            type: 'https://www.google.com.mx/maps/@19.6308773,-99.1367803,3a,75y,195.59h,90t/data=!3m5!1e1!3m3!1sViqkrI4BQ4zQ1iCz2VZeHA!2e0!6s%2F%2Fgeo3.ggpht.com%2Fcbk%3Fpanoid%3DViqkrI4BQ4zQ1iCz2VZeHA%26output%3Dthumbnail%26cb_client%3Dsearch.TACTILE.gps%26thumb%3D2%26w%3D96%26h%3D64%26yaw%3D195.59064%26pitch%3D0%26thumbfov%3D100',
+                            messenger_extension: 'FALSE',
+                            webview_height_ratio:'COMPACT'
+                        },
+                        buttons: [{
+                            type: 'web_url',
+                            url: 'https://www.google.com.mx/maps/@19.6308773,-99.1367803,3a,75y,195.59h,90t/data=!3m5!1e1!3m3!1sViqkrI4BQ4zQ1iCz2VZeHA!2e0!6s%2F%2Fgeo3.ggpht.com%2Fcbk%3Fpanoid%3DViqkrI4BQ4zQ1iCz2VZeHA%26output%3Dthumbnail%26cb_client%3Dsearch.TACTILE.gps%26thumb%3D2%26w%3D96%26h%3D64%26yaw%3D195.59064%26pitch%3D0%26thumbfov%3D100',
+                            title:'mostrar el mapa'
+                        },
+                            {
+                                type: 'phone_number',
+                                title: 'llamar a la tienda',
+                                payload:'+525531077600'
+                            }
+
+                        ]
+                    }
+                    ]
+                }
+            }
+        }
+    }
+    sendAPI.callSendAPI(response);
+}
