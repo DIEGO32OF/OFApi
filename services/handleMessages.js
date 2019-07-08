48624c5d6da9325ef028e1dd8c06dbdd06ab842d
diego <32820519+DIEGO32OF@users.noreply.github.com>
2019-07-08 15:04:22 -0500

    Update handleMessages.js

--- services/handleMessages.js
+++ services/handleMessages.js
@@ -5,6 +5,7 @@ exports.handleMessage = (webhookEvent) => {
         let mensaje = webhookEvent.message;
         if (mensaje.quick_reply) {
             console.log('envio respuesta rapida como un clic boton');
+            handlequickReplies(webhookEvent);
         }
         else if (mensaje.attachments) {
             console.log('se envio un adjunto');
@@ -38,4 +39,28 @@ handlePostback = (webhookEvent) => {
             break;
 
     }
+}
+
+handlequickReplies = (webhookEvent) => {
+    let reply = webhookEvent.message.quick_reply.payload;
+    const response = {
+        texto: 'Nos recomendarias?',
+        replies: [{
+            content_type: 'text',
+            title: 'Si',
+            payload: 'sirecomiendo'
+        },
+        {
+            content_type: 'text',
+            title: 'No',
+            payload: 'Norecomiendo'
+        }]
+    }
+    if (reply == 'servicio' || reply == 'rapidez' || reply == 'rapidez' || reply == 'ubicacion') {
+        actions.quickReplies(webhookEvent, response);
+    }
+    else {
+        actions.sendTextMessage('Gracias por ayudarnos a mejorar', webhookEvent);
+    }
+
 }
