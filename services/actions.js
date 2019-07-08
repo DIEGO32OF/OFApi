b8beb32c6f9b25c9576ba28cf7ce3c030b8f3405
diego <32820519+DIEGO32OF@users.noreply.github.com>
2019-07-08 14:30:59 -0500

    Update actions.js

--- services/actions.js
+++ services/actions.js
@@ -1,5 +1,44 @@
 const sendAPI = require('./graphAPI');
 
+
+
+const repliesSurvey = {
+    texto: 'por favor contesta la siguiente encuesta y dime que es lo que mas te gusto de nuestro servicio',
+    replies: [
+        {
+            content_type: 'text',
+            title: 'Servicios',
+            payload:'servicio'
+        },
+        {
+            content_type: 'text',
+            title: 'Rapidez',
+            payload: 'rapidez'
+        },
+        {
+            content_type: 'text',
+            title: 'ubicacion',
+            payload: 'ubicacion'
+        },
+    ]
+}
+
+exports.quickReplies = (webhookEvent, replies) => {
+    if (!replies) {
+        replies = repliesSurvey;
+    }
+    let response = {
+        recipient = {
+            id: webhookEvent.sender.id
+        },
+        message: {
+            text: replies.texto,
+            quick_replies: replies.replies
+        }
+    }
+    sendAPI.callSendAPI(response);
+}
+
 exports.sendTextMessage = (texto, webhookEvent) => {
     let response = {
         recipient:{
