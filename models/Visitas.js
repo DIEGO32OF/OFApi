9e867b2624d93dd79ec3bf437df410919fbda17a
diego <32820519+DIEGO32OF@users.noreply.github.com>
2019-11-27 18:03:10 -0600

    Update Visitas.js

--- models/Visitas.js
+++ models/Visitas.js
@@ -2,14 +2,17 @@
 var mongoose = require('mongoose');
 
 var esquema = mongoose.Schema;
-var VisitasSchema = esquema({
-
-   // estatus: String,
+var VisitasSchema = esquema({   
     local: String,
     Fecha_Creada: String,
     Origen: String,
     IsActive: String,
-   
+    fullpicture1: String,
+    fullpicture2: String,
+    fullpicture3: String,
+    imageType1 :{ type: esquema.ObjectId, ref: 'advertising' },
+    imageType2 :{ type: esquema.ObjectId, ref: 'advertising' },
+    imageType3 :{ type: esquema.ObjectId, ref: 'advertising' }
 
 
 });
