082abec6704ee6353e78966a75f005646d054d18
diego <32820519+DIEGO32OF@users.noreply.github.com>
2020-03-05 18:58:16 -0600

    Create SendFb.js

---
+++ services/SendFb.js
@@ -0,0 +1,44 @@
+use strict'
+
+const admin = require('firebase-admin')
+const serviceAccount = require('./accesKey.json')
+
+if(admin.apps.length === 0) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://habits-ai.firebaseio.com'
+  })
+  }
+
+ let sentToFirebase = (Tokens,  admin, title, body, ) => {
+
+  
+
+  const payload =  {notification: {title: title, body: body}}
+
+  var options = {
+    priority: 'high',
+    timeToLive: 60 * 60 * 24
+  }
+
+  var result=''
+  
+  return admin.messaging().sendToDevice(Tokens, payload, options) 
+ /*   .then(JSON.stringify)  
+   .catch(JSON.stringify)  */ 
+   .then(response => {
+    console.log('Successfully sent message:', response);
+    result=JSON.stringify(response)
+
+    /* return new Promise(function (resolve,reject){
+      resolve(response)
+    })
+     */
+    return result;
+})
+.catch(function(error) {
+  console.log('Error sending message:', error);
+  result=JSON.stringify(error)
+})
+  
+}
