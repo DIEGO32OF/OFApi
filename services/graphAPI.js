1a287da73d943225853bbe6ff7ad97e099095fad
DIEGO32OF <drivas.dica@gmail.com>
2020-07-02 21:42:35 -0500

    change params

--- services/graphAPI.js
+++ services/graphAPI.js
@@ -28,17 +28,17 @@ exports.getProfile= async (senderID)=>{
     const url=`https://graph.facebook.com/v3.3/${senderID}`;
     let tokenAcces = 'EAAJqBwwGCjQBABt4xFJNEZAJooZCEPNN1jtBQKsu3mY5QgKb9ps7HlbsySYFPrdP6vsBz4eOhKABvOLrgZBsjzUTG2LaCTGEW2D2wd5EkMENzZAYDF7rlmljETQF3EuSZB7hwhsiDS9zwOOkmZBZBLo9OEsfWgY8uGa1zl4e8xpu9xDC8wo1QDE'
 
-    const bodyParameters = {
-        qs:{
+    const qs = {
+        
             "access_token": tokenAcces,
             "fields":'first_name,last_name,gender,locale,timezone'            
-        }
+        
     
     };
-    axios.get(url, bodyParameters).then( res => {
+    axios.get(url, qs).then( res => {
         console.log(res,'///////////]]]]]]]]]]]]')
         return res
-    }).catch(console.log);
+    }).catch(console.log(err));
 
    
     
