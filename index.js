29547d533630af5e4e6ba7ad7aacf5523a2382ed
diego <32820519+DIEGO32OF@users.noreply.github.com>
2022-10-12 14:13:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ io.on('connection', function (socket) {
 //mongoose.connect('mongodb://DiegoR:SALINIDAD1*@ds231568.mlab.com:31568/heroku_7xdfk9c8', (err,res) => {
     //mongodb+srv://Diego35:DEFTONES1*@cluster0.mitn0.mongodb.net/ofDB?retryWrites=true&w=majority
     //actual    mongodb://DiegoR:DEFTONES1*@ds245210.mlab.com:45210/heroku_bql92j6t
-mongoose.connect('mongodb+srv://diego1:DEFTONES1*@cluster0.vmzh1.mongodb.net/?retryWrites=true&w=majority', (err,res) => {
+mongoose.connect('mongodb://diego1:DEFTONES1*@cluster0.vmzh1.mongodb.net/?retryWrites=true&w=majority', (err,res) => {
     if(err)
     {        
     throw err;
